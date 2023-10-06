af2ff8770d1e19d19ef8c7460ac952cf2b20d55d
Gabriel Silva <gsilva_1997@hotmail.com>
2023-10-06 20:34:21 -0300

    innitial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let Leonardo = window.document.getElementById("Leonardo")
+let Samantha = window.document.getElementById("Samantha")
+let Bruna = window.document.getElementById("Bruna")
+let setaDireita = window.document.getElementById("seta-direita")
+let setaEsquerda = window.document.getElementById("seta-esquerda")
+
+function rolarParaDireita(){
+  Leonardo.style = 'display:none'
+  Bruna.style = 'display:flex'
+  setaDireita.style = 'display:none'
+  setaEsquerda.style = 'display:flex; margin-top: 55px'
+}
+
+function rolarParaEsquerda(){
+  Leonardo.style = 'display:flex'
+  Bruna.style = 'display:none'
+  setaDireita.style = 'display:flex; margin-top:55px'
+  setaEsquerda.style = 'display:none'
+}
+
